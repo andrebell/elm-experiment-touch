f60a1b920b7ceb35704b75805be7f2e103b18889
Andre Alexander Bell <github@andre-bell.de>
2020-05-22 07:58:29 +0200

    Updates

---
+++ precache-manifest.ca4ad3d6c26aa9dedc772d6b1cc80db1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "987da976a8b2a4169346a497ba84749d",
+    "url": "/elm-experiment-touch/favicon.ico"
+  },
+  {
+    "revision": "fcdbda457092ac48d2d6b5f262d62ee4",
+    "url": "/elm-experiment-touch/index.html"
+  },
+  {
+    "revision": "6ead519905b7b907c00965be37dd5cd7",
+    "url": "/elm-experiment-touch/logo.svg"
+  },
+  {
+    "revision": "1c82903c2994c2507b385b9042bbfac5",
+    "url": "/elm-experiment-touch/manifest.json"
+  },
+  {
+    "revision": "8636b54c56ad57c05e30",
+    "url": "/elm-experiment-touch/static/css/main.7f457dfb.chunk.css"
+  },
+  {
+    "revision": "8636b54c56ad57c05e30",
+    "url": "/elm-experiment-touch/static/js/main.8636b54c.chunk.js"
+  },
+  {
+    "revision": "481da8033775ff5f42d6",
+    "url": "/elm-experiment-touch/static/js/runtime~main.481da803.js"
+  },
+  {
+    "revision": "7aa09ec89c957a98c55b",
+    "url": "/elm-experiment-touch/static/js/vendors~main.7aa09ec8.chunk.js"
+  }
+]);
