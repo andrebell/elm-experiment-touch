fd4f1df1b5d50270cb27a5f97284280489d10f33
Andre Alexander Bell <github@andre-bell.de>
2020-05-22 07:18:54 +0200

    full build at docs folder

---
+++ docs/precache-manifest.bb790693131b219911c1c4cb4c4faac4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "987da976a8b2a4169346a497ba84749d",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "d76a1ba49e57f615c4b6bdc96a514e93",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ead519905b7b907c00965be37dd5cd7",
+    "url": "/logo.svg"
+  },
+  {
+    "revision": "1c82903c2994c2507b385b9042bbfac5",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "f99f92b6ee591395e90a",
+    "url": "/static/css/main.7f457dfb.chunk.css"
+  },
+  {
+    "revision": "f99f92b6ee591395e90a",
+    "url": "/static/js/main.f99f92b6.chunk.js"
+  },
+  {
+    "revision": "1b922744246c39b6bc43",
+    "url": "/static/js/runtime~main.1b922744.js"
+  },
+  {
+    "revision": "7aa09ec89c957a98c55b",
+    "url": "/static/js/vendors~main.7aa09ec8.chunk.js"
+  }
+]);
